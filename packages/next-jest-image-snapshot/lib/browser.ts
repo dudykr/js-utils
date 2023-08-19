07d2822f9c682f4532f0906ebb0b5dfc764d806f
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-20 02:37:58 +0900

    more fields

--- packages/next-jest-image-snapshot/lib/browser.ts
+++ packages/next-jest-image-snapshot/lib/browser.ts
@@ -1,8 +1,12 @@
 import { NextTestServer } from "./next-server";
+import { ThenableWebDriver } from "selenium-webdriver";
 
 /**
  * An instance of browser which is bound to a [NextTestServer]
  */
 export class BrowserInstance {
-  constructor(private readonly server: NextTestServer) {}
+  constructor(
+    private readonly server: NextTestServer,
+    private readonly driver: Awaited<ThenableWebDriver>,
+  ) {}
 }
