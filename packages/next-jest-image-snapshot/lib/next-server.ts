eb4a0438fa1ed773b49673f7bf0e088edb0f9341
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-20 01:44:04 +0900

    random port

--- packages/next-jest-image-snapshot/lib/next-server.ts
+++ packages/next-jest-image-snapshot/lib/next-server.ts
@@ -1,10 +1,14 @@
 import { NextServer, NextServerOptions } from "next/dist/server/next";
+import { getRandomInt } from "./util";
 
 export class NextTestServer {
   private constructor(private next: NextServer) {}
 
-  public static async create(options: NextServerOptions) {
-    const next = new NextServer(options);
+  public static async create(options: Exclude<NextServerOptions, "port">) {
+    const next = new NextServer({
+      ...options,
+      port: getRandomInt(10000, 65000),
+    });
 
     return new NextTestServer(next);
   }
