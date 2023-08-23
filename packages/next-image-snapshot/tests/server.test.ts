531d1ad0daed046d354a802fe85a984785232984
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-24 03:03:46 +0900

    Add a test

---
+++ packages/next-image-snapshot/tests/server.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, beforeAll } from "@jest/globals";
+import { NextTestServer } from "../lib/next-server";
+
+describe("NextTestServer", () => {
+  let server!: NextTestServer;
+
+  beforeAll(async () => {
+    server = await NextTestServer.create();
+  });
+
+  describe("[Symbol.dispose]", () => {
+    it("works with using", () => {
+      console.log(`Ur: ${server.getUrl("/")}`);
+    });
+  });
+});
