4cd694b5b25b67f3855167411bc38ca38c2f1c60
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-24 03:58:36 +0900

    `spawnAsync`

--- packages/next-image-snapshot/lib/util.ts
+++ packages/next-image-snapshot/lib/util.ts
@@ -1,5 +1,10 @@
+import { promisify } from "util";
+import { spawn } from "child_process";
+
 export function getRandomInt(min: number, max: number) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+export const spawnAsync = promisify(spawn);
