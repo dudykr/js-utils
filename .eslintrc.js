3bc80532039c69878426235bc4cebdfe8fe3a3db
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-24 05:03:28 +0900

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,6 @@ module.exports = {
   plugins: ["@typescript-eslint", "react"],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
   },
 };
