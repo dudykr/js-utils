19c02bfbc51b9b6aa3a799e65321960753704ba2
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-24 15:32:32 +0900

    font

--- packages/next-image-snapshot/examples/next-app/app/layout.tsx
+++ packages/next-image-snapshot/examples/next-app/app/layout.tsx
@@ -1,4 +1,7 @@
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -7,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
