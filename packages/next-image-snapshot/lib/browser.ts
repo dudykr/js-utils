8f681c8d050ecfd1f794c701c294655f36706b1f
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-08-24 12:31:51 +0900

    Browser APIs

--- packages/next-image-snapshot/lib/browser.ts
+++ packages/next-image-snapshot/lib/browser.ts
@@ -1,15 +1,33 @@
 import { RenderedPage } from "lib";
 import { NextTestServer } from "./next-server";
-import { ThenableWebDriver } from "selenium-webdriver";
+import { Builder, ThenableWebDriver } from "selenium-webdriver";
 
 /**
  * An instance of browser which is bound to a [NextTestServer]
  */
-export class BrowserInstance {
+export class Browser {
   constructor(
     private readonly server: NextTestServer,
     private readonly driver: Awaited<ThenableWebDriver>,
   ) {}
+
+  public static async create(
+    server: NextTestServer,
+    browser: string,
+  ): Promise<Browser> {
+    const driver = await new Builder().forBrowser(browser).build();
+
+    return new Browser(server, driver);
+  }
+
+  public static async all(
+    server: NextTestServer,
+    browsers: string[],
+  ): Promise<Browser[]> {
+    return Promise.all(
+      browsers.map((browser) => Browser.create(server, browser)),
+    );
+  }
 
   public async render(pathname: string): Promise<RenderedPage> {
     console.log(`Rendering ${pathname}...`);
